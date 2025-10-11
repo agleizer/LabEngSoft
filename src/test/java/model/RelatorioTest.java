2975d5a6812aa6a0e2363b80bda0a4f101bb33e2
Caio Corsini <98825767+caiocorsini@users.noreply.github.com>
2025-10-11 11:31:44 -0300

    Classes originais modificadas para conectar ao BD

--- src/test/java/model/RelatorioTest.java
+++ src/test/java/model/RelatorioTest.java
@@ -1,22 +1,26 @@
 package model;
 
 import org.junit.jupiter.api.Test;
-import java.util.Date;
+import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RelatorioTest {
 
     private Avaliacao criarAvaliacao(float nota) {
-        return new Avaliacao(nota, 0, 0, 0, 0, 0, "Comentário", false, false, false, false);
+        LocalDate data = LocalDate.of(2024, 9, 25); // fixed test date
+        return new Avaliacao(nota, 0, 0, 0, 0, data,
+                             "Comentário", false, false, false, false);
     }
 
     @Test
     void testConstrutorBasico() {
-        Date data = new Date();
+        LocalDate data = LocalDate.of(2024, 10, 11);
         Relatorio relatorio = new Relatorio(data, "Zona Central", "Gilberto", "4.2");
         
         assertEquals(data, relatorio.getDataGeracaoRelatorio());
         assertEquals("Zona Central", relatorio.getArea());
+        assertEquals("Gilberto", relatorio.getAutor());
+        assertEquals("4.2", relatorio.getMediaNotas());
     }
 
     @Test
