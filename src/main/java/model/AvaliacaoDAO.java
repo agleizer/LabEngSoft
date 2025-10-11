57fa95d3d9529031bcc725b7a819ae244278c066
Caio Corsini <98825767+caiocorsini@users.noreply.github.com>
2025-10-11 15:36:27 -0300

    Classes DAO de interface com banco de dados

---
+++ src/main/java/model/AvaliacaoDAO.java
@@ -0,0 +1,58 @@
+package model;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AvaliacaoDAO {
+
+    public void create(Avaliacao a, long calcadaId) throws SQLException {
+        String sql = """
+            INSERT INTO avaliacao (calcada_id, nota_geral, nota_idoso, nota_cego,
+            nota_cadeirante, nota_carrinho, data_aval, comentario,
+            presenca_piso_tatil, rebaixamento_guia, sem_obstaculos, iluminacao_noturna)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """;
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setLong(1, calcadaId);
+            stmt.setFloat(2, a.getNotaGeral());
+            stmt.setFloat(3, a.getNotaIdoso());
+            stmt.setFloat(4, a.getNotaCego());
+            stmt.setFloat(5, a.getNotaCadeirante());
+            stmt.setFloat(6, a.getNotaCarrinho());
+            stmt.setDate(7, Date.valueOf(a.getDataAval()));
+            stmt.setString(8, a.getComentario());
+            stmt.setBoolean(9, a.isPresencaPisoTatil());
+            stmt.setBoolean(10, a.isRebaixamentoGuia());
+            stmt.setBoolean(11, a.isSemObstaculos());
+            stmt.setBoolean(12, a.isIluminacaoNoturna());
+            stmt.executeUpdate();
+        }
+    }
+
+    public List<Avaliacao> readAll() throws SQLException {
+        List<Avaliacao> list = new ArrayList<>();
+        String sql = "SELECT id, nota_geral, comentario FROM avaliacao";
+        try (Connection conn = DatabaseConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                Avaliacao a = new Avaliacao();
+                a.setNotaGeral(rs.getFloat("nota_geral"));
+                a.setComentario(rs.getString("comentario"));
+                list.add(a);
+            }
+        }
+        return list;
+    }
+
+    public void delete(long id) throws SQLException {
+        String sql = "DELETE FROM avaliacao WHERE id = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setLong(1, id);
+            stmt.executeUpdate();
+        }
+    }
+}
--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -1,70 +1,19 @@
 package model;
 
-import java.sql.*;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        try (Connection conn = DatabaseConnection.getConnection()) {
-            System.out.println("Connected to PostgreSQL!");
+        try {
+            RuaDAO ruaDAO = new RuaDAO();
+            Rua rua = new Rua("Rua das Flores", "Centro");
+            ruaDAO.create(rua);
+            System.out.println("Rua criada!");
 
-            // --- CREATE Rua ---
-            String insertRua = "INSERT INTO rua(nome, bairro) VALUES (?, ?)";
-            long ruaId = 0;
-            try (PreparedStatement pstmt = conn.prepareStatement(insertRua, Statement.RETURN_GENERATED_KEYS)) {
-                pstmt.setString(1, "Rua das Flores");
-                pstmt.setString(2, "Centro");
-                int rows = pstmt.executeUpdate();
-                System.out.println("Rows inserted into rua: " + rows);
+            List<Rua> ruas = ruaDAO.readAll();
+            ruas.forEach(r -> System.out.println("Rua: " + r.getNome() + ", Bairro: " + r.getBairro()));
 
-                ResultSet keys = pstmt.getGeneratedKeys();
-                if (keys.next()) ruaId = keys.getLong(1);
-            }
-
-            // --- CREATE Calcada ---
-            String insertCalcada = "INSERT INTO calcada(nome, avaliacao_media, latitude_ini, longitude_ini, latitude_fim, longitude_fim, rua_id, bairro) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-            try (PreparedStatement pstmtCalc = conn.prepareStatement(insertCalcada)) {
-                pstmtCalc.setString(1, "Calçada Central");
-                pstmtCalc.setFloat(2, 4.5f);
-                pstmtCalc.setDouble(3, -23.5);
-                pstmtCalc.setDouble(4, -46.6);
-                pstmtCalc.setDouble(5, -23.6);
-                pstmtCalc.setDouble(6, -46.7);
-                pstmtCalc.setLong(7, ruaId);
-                pstmtCalc.setString(8, "Centro");
-                int calcRows = pstmtCalc.executeUpdate();
-                System.out.println("Rows inserted into calcada: " + calcRows);
-            }
-
-            // --- READ Calcadas ---
-            String selectCalcadas = "SELECT id, nome, avaliacao_media, bairro FROM calcada";
-            try (Statement stmt = conn.createStatement()) {
-                ResultSet rs = stmt.executeQuery(selectCalcadas);
-                while (rs.next()) {
-                    System.out.println("Calcada ID: " + rs.getLong("id") +
-                                       ", Nome: " + rs.getString("nome") +
-                                       ", Avaliacao Media: " + rs.getFloat("avaliacao_media") +
-                                       ", Bairro: " + rs.getString("bairro"));
-                }
-            }
-
-            // --- UPDATE Rua ---
-            String updateRua = "UPDATE rua SET bairro = ? WHERE nome = ?";
-            try (PreparedStatement pstmt = conn.prepareStatement(updateRua)) {
-                pstmt.setString(1, "Bairro Atualizado");
-                pstmt.setString(2, "Rua das Flores");
-                int updated = pstmt.executeUpdate();
-                System.out.println("Rows updated in rua: " + updated);
-            }
-
-            // --- DELETE Rua ---
-            String deleteRua = "DELETE FROM rua WHERE nome = ?";
-            try (PreparedStatement pstmt = conn.prepareStatement(deleteRua)) {
-                pstmt.setString(1, "Rua das Flores");
-                int deleted = pstmt.executeUpdate();
-                System.out.println("Rows deleted in rua: " + deleted);
-            }
-
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
